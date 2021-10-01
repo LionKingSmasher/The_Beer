7bb380520bd688ed5b3b5b6baddb401f55faebd5
Shin Hyun Kyu <shk052353@gmail.com>
2021-10-01 16:46:49 +0900

    Update Library 20211001

--- src/beer_sock.cc
+++ src/beer_sock.cc
@@ -23,6 +23,7 @@ BeerSock::BeerSock(const char * address, uint16_t port){
 #define o(X) memset(&X, 0, sizeof(X))
 	o(this->serverAddr);
 	o(this->clntAddr);
+	o(this->otherServerAddr);
 #undef o
 	this->serverAddr.sin_family = AF_INET;
 	this->serverAddr.sin_port = htons(port);
@@ -37,21 +38,22 @@ BeerSockStatus_t BeerSock::server_start(){
 #if DEBUG == 1
 	const char* serverMessage = "Hello, Beer!";
 #endif
-	int waitStatus;
+	socklen_t clntSockSize;
+	server_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
 	serverProc = fork();
 	if(serverProc == 0){
-		socklen_t clntSockSize;
-		server_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 		if(server_sock == -1)
 			error_msg("socket() error", -1);
 
 		if(bind(server_sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1)
 			error_msg("bind() error", -1);
 
+		printf("listen()....\n");
 		if(listen(server_sock, 5) == -1)
 			error_msg("listn() error", -1);
 
-		printf("listen...");
+		printf("accrpt()....\n");
 		clntSockSize = sizeof(clntAddr);
 		other_clnt_sock = accept(server_sock, (struct sockaddr *)&clntAddr, &clntSockSize);
 		if(other_clnt_sock == -1)
@@ -61,11 +63,7 @@ BeerSockStatus_t BeerSock::server_start(){
 		close(other_clnt_sock);
 		close(server_sock);
 #endif
-		exit(0);
 	}
-	wait(&waitStatus);
-	if(WEXITSTATUS(waitStatus) == -1)
-		return BEERSOCK_FAIL;
 	return BEERSOCK_SUCCESS;
 }
 
@@ -86,7 +84,8 @@ BeerSockStatus_t BeerSock::connectServer(const char* ip, uint16_t port){
 		error_msg("socket error", -1);
 	otherServerAddr.sin_family = AF_INET;
 	otherServerAddr.sin_addr.s_addr=inet_addr(ip);
-	otherServerAddr.sin_port = port;
+	otherServerAddr.sin_port = htons(port);
+	printf("%x %x\n", otherServerAddr.sin_port, port);
 	if(connect(my_clnt_sock, (struct sockaddr*)&otherServerAddr, sizeof(otherServerAddr)) == -1)
 		error_msg("connect error", -1);
 #if DEBUG == 1
--- testProgram/20210929/main.cc
+++ testProgram/20210929/main.cc
@@ -2,7 +2,8 @@
 #include <beer.h>
 
 int main(int argc, char** argv){
-	BeerSock p2pSock("10.80.163.30", 9999);
-	p2pSock.server_start();
+	BeerSock p2pSock("10.80.163.30", 7777);
+	//p2pSock.server_start();
+	p2pSock.connectServer("10.80.163.172", 9999);
 	return 0;
 }
